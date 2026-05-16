acd524902ef6a328625e330418dfe916404afc52
theophil-kroller <theophil.kroller@gmail.com>
2026-05-16 22:16:26 +0000

    Deploying to gh-pages from @ theophil-kroller/theophil-kroller.github.io@cb8dfcb017baab08f867768bd090f6393476769b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -419,9 +419,9 @@ ninja.data = [{
           description: "Sustainable business models for SMEs",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-unite-pre-incubation-program",
+          title: 'UNITE! Pre-Incubation Program',
+          description: "International entrepreneurship education across the UNITE! alliance",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
