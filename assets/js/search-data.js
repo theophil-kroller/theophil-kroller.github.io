22d6d127a696cf720004dfd03a0a208604704c6d
theophil-kroller <theophil.kroller@gmail.com>
2026-05-16 22:05:08 +0000

    Deploying to gh-pages from @ theophil-kroller/theophil-kroller.github.io@b93bab81656aa44d0cd24e6bfc850aea41cfbc64 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -409,9 +409,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-doctoral-research",
+          title: 'Doctoral Research',
+          description: "Strategic decision-making and business model adaptation in sustainability transitions",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
