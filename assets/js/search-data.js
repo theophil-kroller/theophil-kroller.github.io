a481a11e64bb8b2339af2eae9685e45d4a9b51a7
theophil-kroller <theophil.kroller@gmail.com>
2026-05-16 19:21:15 +0000

    Deploying to gh-pages from @ theophil-kroller/theophil-kroller.github.io@5f0d274f1ceebe4289605aeda98d87c13536589d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "Teaching philosophy, courses, methods, and selected formats.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
