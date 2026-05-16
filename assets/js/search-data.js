70edf476865aa96b0d5bd4f814f75c0adce48ad8
theophil-kroller <theophil.kroller@gmail.com>
2026-05-16 22:06:51 +0000

    Deploying to gh-pages from @ theophil-kroller/theophil-kroller.github.io@d3114c4568514828b0d74a788fd136b9db3c36d3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -414,9 +414,9 @@ ninja.data = [{
           description: "Strategic decision-making and business model adaptation in sustainability transitions",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-unite-pre-incubation-program",
+          title: 'UNITE! Pre-Incubation Program',
+          description: "International entrepreneurship education across the UNITE! alliance",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
